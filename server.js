f0dd99ffdc4160df87a1de36020d406bfd486994
hurmain7 <hurmain7@gmail.com>
2022-03-02 19:40:04 +0500

    readys

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
-
-connectDB();
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use(express.json({ extended: false}))
 
